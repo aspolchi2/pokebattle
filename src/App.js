f39eed1b2d10f0a003afa226303dd011ff4e2422
aspolchi <aspolchi@gmail.com>
2022-08-12 12:20:24 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import "./App.css";
-import './sass/global.scss'
+import "./sass/global.scss";
 import PokeContainer from "./containers/PokeContainer";
 import { PokeProvider } from "./context/PokeContext";
 
 function App() {
   return (
     <PokeProvider>
-    <div className="title">
-      <h1>Weaker or Stronger ?</h1>
-    </div>
-      <PokeContainer/>
+      <div className="title">
+        <h1>Weaker or Stronger ?</h1>
+      </div>
+      <PokeContainer />
     </PokeProvider>
-      
   );
 }
 
