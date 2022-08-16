adaad32aba953e986741ff3e4aec2228a53e099a
Agustin Somoza Polchi <aspolchi@gmail.com>
2022-08-16 17:34:37 -0300

    Update Poke.js

--- src/components/Poke.js
+++ src/components/Poke.js
@@ -1,20 +1,25 @@
 import React, { useEffect } from "react";
 import "../sass/poke.scss";
+import { Zoom, Fade,Flip, AttentionSeeker, JackInTheBox, Hinge, Reveal, Slide, Bounce } from "react-awesome-reveal";
 
-const Poke = ({ name, sprites, pokeAttack }) => { 
+
+const Poke = ({ name, sprites, pokeAttack }) => {
 
   return (
+    <Bounce >
     <div className="pokemon">
       <h1 className="pokemon__name">{name}</h1>
       <img
         className="pokemon__img"
-        src={sprites ? sprites.front_default : null}
+        src={sprites?.front_default}
         alt={name}
       />
       <p className="pokemon__attack">
         Base Attack = {pokeAttack ? pokeAttack : "??"}
       </p>
     </div>
+
+    </Bounce>
   );
 };
 
